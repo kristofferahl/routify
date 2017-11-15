f67bcc18bc9b8691aef1c820aeeb972362fe7c1c
Kristoffer Ahl <git@77dynamite.com>
2017-11-15 16:44:03 +0100

    - Updated example and applied Prettier formatting.

--- examples/stack/index.js
+++ examples/stack/index.js
@@ -1,16 +1,20 @@
-var http = require('http');
-var stack = require('stack');
-var route = require('./../../routify.js');
+var http = require('http')
+var Stack = require('stack')
+var router = require('./../../routify.js')
 
 var routes = require('./routes/index.js')
 
-http.createServer(stack(
-	route('/', routes.index),
-	route.get('/signup', routes.signup.get),
-	route.post('/signup', routes.signup.post),
-	route.get('/documents/:id', routes.documents.get),
-	route.get('/stuff/:userId', function (req, res, next) {
-		console.log('Accessed user stuff', req.params.userId);
-		res.end("Here's you're stuff - " + req.params.userId);
-	})
-)).listen(1337);
+http
+  .createServer(
+    Stack(
+      router.get('/', routes.index),
+      router.get('/signup', routes.signup.get),
+      router.post('/signup', routes.signup.post),
+      router.get('/documents/:id', routes.documents.get),
+      router.get('/stuff/:userId', function (req, res, next) {
+        console.log('Accessed user stuff', req.params.userId)
+        res.end("Here's you're stuff - " + req.params.userId)
+      })
+    )
+  )
+  .listen(1337)
